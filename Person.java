7c443d817cafb7a5f80d40c1f2501a581d4918c7
Marius Gundersen <gundersen@gmail.com>
2013-04-15 18:40:11 +0200

    added Person.java class

---
+++ Person.java
@@ -0,0 +1,27 @@
+class Person {
+	private String name;
+	private int age;
+	
+	public Person(){
+	
+	}
+	
+	public void setName(String name){
+		this.name = name;
+	}
+	
+	public String getName(){
+		return name;
+	}
+	
+	public void setAge(int age){
+		this.age = age;
+	}
+	
+	public int getAge(){
+		return age;
+	}
+	
+	public String getEpost() {
+	}
+}
